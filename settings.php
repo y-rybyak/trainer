a1bf64064b3d6f036f5048765be1151a19839e4b
Yaroslav Ribyak <y4@miritec.com>
2015-04-14 16:51:42 +0300

    new features

--- settings.php
+++ settings.php
@@ -13,28 +13,45 @@ $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 <div class="row">
     <div class="col-md-4"></div>
     <div class="col-md-4">
-        <h1><a href="/logout.php">Log out</a></h1>
-        <table class="table table-striped">
-            <tr>
-                <th>Select</th>
-                <th>Russian</th>
-                <th>English</th>
-            </tr>
-            <?php
-            foreach ($result as $val) {
-                print "<tr>";
-                print "<td>".'<input type="checkbox" checked>'."</td>"."<td>" . $val["russian"] . "</td>" . "<td>" . $val["english"] . "</td>";
-                print "</tr>";
-            }
-            ?>
-        </table>
+        <div class="centered-text">
+            <h1>Select words to study <small><a href="logout.php">Log out</a></small></h1>
+        </div>
+
         <form name="dictionary" method="POST" action="trainer.php">
+            <table class="table table-striped">
+                <tr>
+                    <th></th>
+                    <th>Russian</th>
+                    <th>English</th>
+                    <th></th>
+                </tr>
+                <?php
+                foreach ($result as $val) {
+                    $word = $val["english"];
+                    print "<tr>";
+                    print "<td><input type=checkbox name=dictionary[] value=$word></td>";
+                    print "<td>" . $val["russian"] . "</td><td>" . $val["english"] . "</td>";
+                    print '<td><span class="glyphicon glyphicon-trash"></td>';
+                    print "</tr>";
+                }
+                ?>
+            </table>
             <div class="form-group">
-                <button type="submit" class="btn btn-default" id="submitWords"
-                        name="submitWords">Start!
+                <button type="submit" class="btn btn-default">Start!
                 </button>
             </div>
+        </form><br />
+        <h3>Add new word to your dictionary!</h3>
+        <form class="form-inline" name="new" method="POST" action="settings.php">
+            <input type="text" class="form-control" id="english" autocomplete="off" placeholder="English word">
+            <input type="text" class="form-control" id="russian" autocomplete="off" placeholder="Russian word">
+            <button type="submit" class="btn btn-default">Add</button>
         </form>
+        <?php
+        if (isset($_POST)) {
+            var_dump($_POST);
+        }
+        ?>
     </div>
     <div class="col-md-4"></div>
 </div>
