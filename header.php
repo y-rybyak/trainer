d7d75da283955007e2c05807fd42cc01b35df4d3
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-04-17 20:59:42 +0300

    favicon

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
     <link href="/css/bootstrap.css" rel="stylesheet">
     <title><?= $title ?></title>
 </head>
@@ -11,7 +12,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12" align="center">
-            <h1><?= $title ?></h1>
+            <h1><?=$title?></h1>
             <hr>
         </div>
     </div>
