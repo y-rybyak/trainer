273046cf576d38236fb7b7adf1d3dfd784f752e9
Yaroslav Rybyak <yaroslav.rybyak@ya.ru>
2015-04-23 23:08:52 +0300

    dump

--- trainer.sql
+++ trainer.sql
@@ -1,30 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Apr 23, 2015 at 10:26 PM
--- Server version: 5.5.43-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `trainer`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `dictionary`
---
+SET time_zone = "+02:00";
 
 CREATE TABLE IF NOT EXISTS `dictionary` (
   `intId` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,23 +8,11 @@ CREATE TABLE IF NOT EXISTS `dictionary` (
   `russian` varchar(255) NOT NULL,
   PRIMARY KEY (`intId`),
   KEY `intUserId` (`intUserId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `guess`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `guess` (
   `varGuess` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
 
 CREATE TABLE IF NOT EXISTS `user` (
   `intId` int(11) NOT NULL AUTO_INCREMENT,
@@ -60,18 +23,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `varLogin` (`varLogin`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
---
--- Dumping data for table `user`
---
-
 INSERT INTO `user` (`intId`, `varLogin`, `varPasswordHash`) VALUES
 (1, 'ihaveabomb', '60d0b59714dcfc8918e9fafea1ac3815');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `words`
---
 
 CREATE TABLE IF NOT EXISTS `words` (
   `intId` int(11) NOT NULL AUTO_INCREMENT,
@@ -82,31 +35,13 @@ CREATE TABLE IF NOT EXISTS `words` (
   KEY `intUserId` (`intUserId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
---
--- Dumping data for table `words`
---
-
 INSERT INTO `words` (`intId`, `intUserId`, `english`, `russian`) VALUES
 (1, 1, 'cat', 'кот'),
 (2, 1, 'forest', 'лес'),
 (3, 1, 'sword', 'меч');
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `dictionary`
---
 ALTER TABLE `dictionary`
   ADD CONSTRAINT `dictionary_ibfk_1` FOREIGN KEY (`intUserId`) REFERENCES `user` (`intId`);
 
---
--- Constraints for table `words`
---
 ALTER TABLE `words`
   ADD CONSTRAINT `words_ibfk_1` FOREIGN KEY (`intUserId`) REFERENCES `user` (`intId`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
